07440a1869f7f8e31cb90b02bd572ab6535cf486
Elisa Battistoni <elisa.battistoni.dev@gmail.com>
2025-06-24 16:45:07 +0200

    fix bug to prisma call to foods

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Welcome } from '@/components';
 import { prisma } from '@/lib/prisma';
 
 export default async function HomePage() {
-  const food = await prisma.food.findMany();
+  const foods = await prisma.foods.findMany();
   // console.log('Fetched food:', food);
   return <Welcome />;
 }
